d21e9e673b0ad710b1a82b0c26ba653d37aa6968
Lucas Calebe <calebematos@gmail.com>
2020-04-13 09:09:40 -0300

    Upadate about me text

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,11 +6,11 @@ export class UserService {
     name: "Lucas Calebe Matos Lopes",
     jobTitle: "Full Stack Developer",
     aboutMe: `
-    I'm a Senior Full Stack Developer with over 5 years of experience developing software with high standards and interacting with multiple 
-    stakeholders from technical and non-technical backgrounds, ensuring clarity, quality, and continuous delivery. 
-    I work with technologies such as Java, Hibernate, SQL, Oracle Databases. I am committed to work and willing to help. 
+    I'm a Senior Full Stack Developer with over 6 years of experience developing software with high standards and interacting with multiple
+    stakeholders from technical and non-technical backgrounds, ensuring clarity, quality, and continuous delivery.
+    I work with technologies such as Java, Hibernate, SQL, Oracle Databases. I am committed to work and willing to help.
     Bachelor's Degree in Information Systems.I love develop and study  software development.
-    In my free time I like to play with my puppy(Totti), watch series and read manga.
+    In my free time I like to play with my puppy(Totti), watch series, read manga and play some games.
    `,
     profilePicture: "./assets/profile.jpg",
     location: "Brazil",
